517ed2142d1b0fc18a2196cab7e9b968cff815f9
Thembisile  Makamu <makamuthe2@gmail.com>
2024-05-02 06:56:03 +0200

    sbl

--- app.js
+++
@@ -1,7 +0,0 @@
-const $container = document.querySelector(".container");
-
-setInterval(() => {
-  const $last = $container.firstElementChild;
-  $last.remove();
-  $container.appendChild($last);
-}, 1000);
